3a58830459c09d1a56d89764d0fa06a3a2b7b631
David <Ninja_Buta@hotmail.fr>
2012-02-25 02:55:03 +0100

    Suppression des .h inutiles

--- initialisationSDL.h
+++
@@ -1,9 +0,0 @@
-#ifndef INITIALISATIONSDL_H_INCLUDED
-#define INITIALISATIONSDL_H_INCLUDED
-
-void initialserSDL();
-void chargerMenu();
-void lireMenu();
-
-
-#endif // INITIALISATIONSDL_H_INCLUDED
--- jeu.h
+++
@@ -1,6 +0,0 @@
-#ifndef JEU_H_INCLUDED
-#define JEU_H_INCLUDED
-
-
-
-#endif // JEU_H_INCLUDED
